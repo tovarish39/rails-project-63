e27174fb915c85e4fb28539cd7b4969e259afd21
gorrus39 <gorrus100@gmail.com>
2024-01-09 17:24:54 +0200

    asdf

--- lib/hexlet_code/block_tag.rb
+++ lib/hexlet_code/block_tag.rb
@@ -1,8 +1,5 @@
 # frozen_string_literal: true
 
-# rubocop:disable Metrics/MethodLength
-# rubocop:disable Metrics/AbcSize
-
 module HexletCode
   # описание модуля
   class BlockTag
@@ -17,21 +14,30 @@ module HexletCode
       value = @model.public_send(tag_name)
       as = options.fetch(:as, nil)
       label_value = tag_name.capitalize.to_s
+      args = [options, tag_name, label_value, value]
       @tags << if as.nil?
-                 input_attributes = { name: tag_name, type: 'text', value: }
-                 input_attributes[:class] = options[:class] unless options[:class].nil?
-
-                 Tag.build('label', for: tag_name) { label_value } +
-                 Tag.build('input', input_attributes)
+                 make_input_with_label(*args)
                elsif as == :text
-                 rows = options.fetch(:rows, 40)
-                 cols = options.fetch(:cols, 20)
-                 attributes = { name: tag_name, cols:, rows: }
-                 attributes[:class] = options[:class] unless options[:class].nil?
-
-                 Tag.build('label', for: tag_name) { label_value } +
-                 Tag.build('textarea', attributes) { value }
+                 make_textarea_with_label(*args)
                end
+    end
+
+    def make_input_with_label(options, tag_name, label_value, value)
+      input_attributes = { name: tag_name, type: 'text', value: }
+      input_attributes[:class] = options[:class] unless options[:class].nil?
+
+      Tag.build('label', for: tag_name) { label_value } +
+        Tag.build('input', input_attributes)
+    end
+
+    def make_textarea_with_label(options, tag_name, label_value, value)
+      rows = options.fetch(:rows, 40)
+      cols = options.fetch(:cols, 20)
+      attributes = { name: tag_name, cols:, rows: }
+      attributes[:class] = options[:class] unless options[:class].nil?
+
+      Tag.build('label', for: tag_name) { label_value } +
+        Tag.build('textarea', attributes) { value }
     end
 
     def submit(value = 'Save')
@@ -40,5 +46,3 @@ module HexletCode
     end
   end
 end
-# rubocop:enable Metrics/MethodLength
-# rubocop:enable Metrics/AbcSize
