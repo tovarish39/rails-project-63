f153bc03712222cc2fc4bcfb7ce0d3df64f3a52c
gorrus39 <gorrus100@gmail.com>
2024-01-09 17:34:35 +0200

    asdf

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -10,8 +10,8 @@ module HexletCode
   autoload :BlockTag, "#{__dir__}/hexlet_code/block_tag.rb"
 
   class << self
-    def form_for(user, options = {}, &block)
-      inner_tags = get_inner_tags(user, &block)
+    def form_for(user, options = {})
+      inner_tags = get_inner_tags(user) { yield } # rubocop:disable Style/ExplicitBlockArgument
 
       action = options.fetch(:url, '#')
       method = options.fetch(:method, 'post')
@@ -21,8 +21,8 @@ module HexletCode
       Tag.build('form', args) { inner_tags }
     end
 
-    def get_inner_tags(user, &block)
-      return nil if block.nil?
+    def get_inner_tags(user)
+      return nil unless block_given?
 
       instance = BlockTag.new(user)
       block.call(instance)
@@ -32,4 +32,28 @@ module HexletCode
       end
     end
   end
+
+  # class << self
+  #   def form_for(user, options = {}, &block)
+  #     inner_tags = get_inner_tags(user, &block)
+
+  #     action = options.fetch(:url, '#')
+  #     method = options.fetch(:method, 'post')
+  #     klass =  options.fetch(:class, nil)
+  #     args = { action:, method: }
+  #     args[:class] = klass unless klass.nil?
+  #     Tag.build('form', args) { inner_tags }
+  #   end
+
+  #   def get_inner_tags(user, &block)
+  #     return nil if block.nil?
+
+  #     instance = BlockTag.new(user)
+  #     block.call(instance)
+  #     instance.tags.reduce('') do |acc, str|
+  #       acc += str
+  #       acc
+  #     end
+  #   end
+  # end
 end
