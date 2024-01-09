0c5881bf8fb4fcd3b1b2a856eca061ed4f936059
gorrus39 <gorrus100@gmail.com>
2024-01-09 19:20:15 +0200

    hexlet-check -> passed

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -34,40 +34,27 @@ module HexletCode
       args[:class] = klass unless klass.nil?
       Tag.build('form', args) { inner_tags }
     end
+
+    # def form_for(user, options = {}, &block)
+    #   inner_tags = get_inner_tags(user, &block)
+
+    #   action = options.fetch(:url, '#')
+    #   method = options.fetch(:method, 'post')
+    #   klass =  options.fetch(:class, nil)
+    #   args = { action:, method: }
+    #   args[:class] = klass unless klass.nil?
+    #   Tag.build('form', args) { inner_tags }
+    # end
+
+    # def get_inner_tags(user, &block)
+    #   return nil if block.nil?
+
+    #   instance = BlockTag.new(user)
+    #   block.call(instance)
+    #   instance.tags.reduce('') do |acc, str|
+    #     acc += str
+    #     acc
+    #   end
+    # end
   end
-
-  # class << self
-  #   def form_for(user, options = {}, &block)
-  #     inner_tags = get_inner_tags(user, &block)
-
-  #     action = options.fetch(:url, '#')
-  #     method = options.fetch(:method, 'post')
-  #     klass =  options.fetch(:class, nil)
-  #     args = { action:, method: }
-  #     args[:class] = klass unless klass.nil?
-  #     Tag.build('form', args) { inner_tags }
-  #   end
-
-  #   def get_inner_tags(user, &block)
-  #     return nil if block.nil?
-
-  #     instance = BlockTag.new(user)
-  #     block.call(instance)
-  #     instance.tags.reduce('') do |acc, str|
-  #       acc += str
-  #       acc
-  #     end
-  #   end
-  # end
-
-  # def get_inner_tags(user)
-  #   return nil unless block_given?
-
-  #   instance = BlockTag.new(user)
-  #   yield instance if block_given?
-  #   instance.tags.reduce('') do |acc, str|
-  #     acc += str
-  #     acc
-  #   end
-  # end
 end
